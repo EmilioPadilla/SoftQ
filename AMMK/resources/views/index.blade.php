7413ef5a1246aed866a77e665de5404e0b05808b
EmilioPadilla <padillam_@hotmail.com>
2020-10-04 11:51:08 -0500

    Agregar framework de laravel, aun no se ha implementado el index

---
+++ AMMK/resources/views/index.blade.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html lang="en" dir="ltr">
+  <head>
+    <meta charset="utf-8">
+    <title>AMMK</title>
+  </head>
+  <body>
+    @section('sidebar')
+            This is the master sidebar.
+    @show
+    <h1>AMMK</h1>
+  </body>
+</html>
