ff2b1c08a11322e36a1de76de9da14422c753c8d
Mariana Favarony Avila <marianafavaronyavila@MacBook-Pro-de-Mariana.local>
2020-10-08 17:08:01 -0500

    registro gobierno 2 terminado

--- AMMK/src/routes.js
+++ AMMK/src/routes.js
@@ -20,7 +20,7 @@ import Notifications from "views/Notifications.js";
 import RE2 from "views/RE2";
 import RE3 from "views/RE3";
 import Rtl from "views/Rtl.js";
-import RDonanteGobierno from "views/RDonanteGobierno";
+import RDonanteGobierno2 from "views/RDonanteGobierno2";
 
 import TableList from "views/TableList.js";
 import Typography from "views/Typography.js";
@@ -73,7 +73,7 @@ var routes = [
     name: "Donantes",
     rtlName: "قائمة الجدول",
     icon: "tim-icons icon-single-02",
-    component: RDonanteGobierno,
+    component: RDonanteGobierno2,
     layout: "/admin"
   },
   
--- AMMK/src/views/RDonanteGobierno2.js
+++ AMMK/src/views/RDonanteGobierno2.js
@@ -5,22 +5,21 @@ import Row from "react-bootstrap/Col";
 import ProgressBar from "react-bootstrap/ProgressBar";
 import Col from "react-bootstrap/Col";
 
-
-class RDonantesPatronato extends Component {
+class RDonanteGobierno2 extends Component {
   render() {
     return (
       <div className="content">
         <div class="container-fluid">
           <h1 className="title">Registrar Donante</h1>
-          
-           <h3 align="center">(Particular/Patronato)</h3>
-        
-          <ProgressBar now={30} />
-                  <br/>
+
+          <h3 align="center">(Gobierno/Empresas/Fundaciones)</h3>
+
+          <ProgressBar now={70} />
+          <br />
           <div class="container"></div>
           <Form>
             <Form.Row>
-              <Form.Group as={Row} controlId="namePatronato">
+              <Form.Group as={Row} controlId="nameGobierno">
                 <Form.Label>Nombre Completo:</Form.Label>
                 <Form.Control
                   type="text"
@@ -28,50 +27,55 @@ class RDonantesPatronato extends Component {
                 />
               </Form.Group>
             </Form.Row>
-
             <Form.Row>
-              <Form.Group as={Row} controlId="birthdayPatronato">
+              <Form.Group as={Row} controlId="cargoGobierno">
+                <Form.Label>Cargo:</Form.Label>
+                <Form.Control type="text" placeholder="VECJ880326 XXX" />
+              </Form.Group>
+            </Form.Row>
+            <Form.Row>
+              <Form.Group as={Row} controlId="birthdayGobierno">
                 <Form.Label>Fecha de Cumpleaños:</Form.Label>
                 <Form.Control type="date" placeholder=" / / " />
               </Form.Group>
             </Form.Row>
 
             <Form.Row>
-              <Form.Group as={Row} controlId="RFCPatronato">
-                <Form.Label>RFC:</Form.Label>
-                <Form.Control type="text" placeholder="VECJ880326 XXX" />
-              </Form.Group>
-            </Form.Row>
-            <Form.Row>
               <Form.Group as={Row} controlId="emailPatronato">
                 <Form.Label>Correo:</Form.Label>
                 <Form.Control type="email" placeholder="ejemplo@ejemplo.com" />
               </Form.Group>
             </Form.Row>
+            
             <Form.Row>
-            <Form.Group as={Row} controlId="telefonoPatronato">
+            <Form.Group as={Row} controlId="telefonoGobierno">
                 <Form.Label>Telefono:</Form.Label>
                 <Form.Control type="text" placeholder="234 3344" />
               </Form.Group>
               &nbsp;&nbsp;&nbsp;
-
-              <Form.Group as={Row} controlId="celularPatronato">
+              <Form.Group as={Row} controlId="celularGobierno">
                 <Form.Label>Celular:</Form.Label>
                 <Form.Control type="text" placeholder="442 343 3233" />
               </Form.Group>
               
-            </Form.Row>
-             
+              </Form.Row>
           </Form>
           <Col align="right">
             <a href="/admin/Facturacion">
-                    <Button className="btn btn-outline-primary" onClick={() => { this.handleClick() }}>Siguiente</Button>
-                  </a>
-                  </Col> 
+              <Button
+                className="btn btn-outline-primary"
+                onClick={() => {
+                  this.handleClick();
+                }}
+              >
+                Siguiente
+              </Button>
+            </a>
+          </Col>
         </div>
       </div>
     );
   }
 }
 
-export default RDonantesPatronato;
+export default RDonanteGobierno2;
