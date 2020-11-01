99b142ca917bcfdd22182c122a51015504df16a5
Sebastián González <sebastafoya@gmail.com>
2020-10-31 19:23:39 -0600

    feat: Init TableIncome component

---
+++ AMMK/src/components/Finance/TableIncomes.js
@@ -0,0 +1,50 @@
+import React from "react";
+import axios from 'axios';
+
+// reactstrap components
+import {
+    Table,
+    Row,
+    Button,
+    Col
+    } from "reactstrap";
+
+class TableIncomes extends React.Component {
+    render() {
+        return (
+            <Row>
+            <Col md="12">
+              <Table hover >
+                  <thead>
+                      <tr>
+                        <th>Fecha</th>
+                        <th>Recibe de</th>
+                        <th>Monto</th>
+                        <th>Folio</th>
+                        <th>Factura</th>
+                      </tr>
+                  </thead>
+                  <tbody>
+                    <tr>
+                      <td>2020-10-10</td>
+                      <td>Fondeo Bancomer</td>
+                      <td>$15,000</td>
+                      <td>57961</td>
+                      <td>FE43243G</td>
+                    </tr>
+                    <tr>
+                      <td>2020-10-10</td>
+                      <td>Fondeo Bancomer</td>
+                      <td>$15,000</td>
+                      <td>57961</td>
+                      <td>FE43243G</td>
+                    </tr>
+                </tbody>
+              </Table>
+            </Col>
+          </Row>
+        )
+    }
+}
+
+export default TableIncomes;
