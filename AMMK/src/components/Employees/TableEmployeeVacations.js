3c65d4c9cb81804a3adc6beb219a5c86aa3cceff
EmilioPadilla <padillam_@hotmail.com>
2020-11-09 00:12:01 -0600

    fix: error dev

--- AMMK/src/components/Employees/TableEmployeeVacations.js
+++ AMMK/src/components/Employees/TableEmployeeVacations.js
@@ -6,8 +6,9 @@
 */
 
 import React from "react";
-import axios from 'axios';
 import { Link } from "react-router-dom";
+import axios from 'axios';
+import { API_BASE_URL } from '../../index';
 
 import SimpleTooltip from "../../views/General/SimpleTooltip";
 
@@ -19,14 +20,12 @@ import {
   Table,
   Row,
   Button,
-  Col
+  Col,
+  Modal, 
+  ModalBody, 
+  ModalFooter
   } from "reactstrap";
 
-  function parseVacations(vacations) {
-    return vacations.map((vacation) => {
-      return { label: vacation.nombre, value: vacation.id };
-    });
-  }
 
   class TableEmployeeVacations extends React.Component {
 
@@ -34,12 +33,42 @@ import {
       super(props)
       this.state = {
         vacations: [],
+        modalEliminar: false,
+        form:{
+          id: '',
+          fechaRegistro: '',
+          fechaSalida: '',
+          fechaRegreso: ''
+    }
       }
     }
 
     componentDidMount() {
       this.getVacations();
     }
+
+
+  peticionDelete=()=>{
+    axios.delete(API_BASE_URL + 'employeeVacations/' + this.state.form.id).then(response=>{
+        console.log(response);
+        console.log(response.data);
+      this.setState({modalEliminar: false});
+
+      const vacations = this.state.vacations.filter(item => item.id !== this.state.form.id);
+    this.setState({ vacations });
+    })
+  }
+
+  seleccionarVacacion=(vacations)=>{
+    this.setState({
+      form: {
+        id: vacations.id,
+        fechaRegistro: vacations.fechaRegistro,
+        fechaSalida: vacations.fechaSalida,
+        fechaRegreso: vacations.fechaRegreso
+      }
+    })
+  }
 
     getVacations() {
       axios.get('http://localhost:8000/api/employeeVacations/1')
@@ -58,7 +87,7 @@ import {
                       <tr>
                         <th>Fecha de registro</th>
                         <th>Fecha de salida</th>
-                        <th>Días</th>
+                        <th>Fecha de regreso</th>
                         <th className="text-center">Acciones</th>
                       </tr>
                   </thead>
@@ -71,22 +100,31 @@ import {
                         <td>
                             <Row>
                             <a href="/admin/view-employee">
-                              <button id="editar" type="button" class="btn btn-info btn-sm">
+                              <button id="editar" type="button" className="btn btn-info btn-sm">
                                 <FontAwesomeIcon icon={['fas', 'edit']} />
                               </button>
                               <SimpleTooltip placement="top" target="editar">Editar registro</SimpleTooltip>
                             </a>
                             &nbsp;&nbsp;&nbsp;&nbsp;
-                              <Button color="danger" size="sm" id="eliminar">
-                              <FontAwesomeIcon icon={['fas', 'trash-alt']} /> </Button>
-                              <SimpleTooltip placement="top" target="eliminar" >Elimina registro</SimpleTooltip>
-                          
+                              <Link>
+                                <SimpleTooltip placement="top" target="eliminar">Eliminar</SimpleTooltip>
+                                <Button size="sm" id="eliminar" color="danger" onClick={()=>{this.seleccionarVacacion(vacation); this.setState({modalEliminar: true})}}><FontAwesomeIcon icon={['fas', 'trash-alt']}/></Button>
+                              </Link>
                             </Row>
                         </td>
                       </tr>
                     ))}
                     </tbody>
               </Table>
+              <Modal isOpen={this.state.modalEliminar}>
+                <ModalBody>
+                   ¿Estás segur@ que deseas eliminar el registro de estas vacaciones?
+                </ModalBody>
+                <ModalFooter>
+                  <Button color="primary"onClick={()=>this.setState({modalEliminar: false})}>No</Button>
+                  <Button color="danger" onClick={()=>this.peticionDelete()}>Sí</Button>
+                </ModalFooter>
+        </Modal>
             </Col>
           </Row>
         );
--- AMMSKLaravel/routes/api.php
+++ AMMSKLaravel/routes/api.php
@@ -18,22 +18,10 @@ Route::middleware('auth:api')->get('/user', function (Request $request) {
     return $request->user();
 });
 
+
+/*DONANTES Y DONACIONES*/
+
 Route::resource('donantes', 'App\Http\Controllers\DonantesController');
-// Rutas empleados
-Route::resource('employee', 'App\Http\Controllers\EmployeesController');
-Route::resource('employeeStatus', 'App\Http\Controllers\StatusEmployeeController');
-Route::resource('scholarship', 'App\Http\Controllers\ScholarshipController');
-Route::resource('status', 'App\Http\Controllers\EmployeesShiftsController');
-Route::resource('employeesShifts', 'App\Http\Controllers\EmployeesShiftsController');
-Route::resource('employeeJobTitles', 'App\Http\Controllers\JobTitleController');
-Route::post('employeesShifts/search', 'App\Http\Controllers\EmployeesShiftsController@filterByEmployee');
-Route::post('employeesShifts/delete', 'App\Http\Controllers\EmployeesShiftsController@deleteByEmployee');
-Route::get('employee/{id}', 'App\Http\Controllers\EmployeesController@show');
-Route::get('employeeVacations/{id}', 'App\Http\Controllers\VacationsController@show');
-Route::get('employeeFiles/{id}', 'App\Http\Controllers\Employee_FilesController@show');
-
-//Rutas cuentas
-
 Route::apiResource('donantes', 'App\Http\Controllers\DonantesController');
 Route::resource('tipodonante', 'App\Http\Controllers\TipoDonanteController');
 Route::resource('recurrencia', 'App\Http\Controllers\RecurrenciaController');
@@ -43,17 +31,46 @@ Route::get('donations/table/all', 'App\Http\Controllers\DonacionController@showT
 Route::get('donors/contact/table/all', 'App\Http\Controllers\ContactoDonanteController@showTable');
 Route::get('donations/showAll/{id}', 'App\Http\Controllers\DonantesController@showDonaciones');
 Route::get('donorContacts/showAll/{id}', 'App\Http\Controllers\DonantesController@showContactos');
-
+Route::get('donations/delete/{id}', 'App\Http\Controllers\DonacionController@deleteD');
+Route::get('donorContacts/delete/{id}', 'App\Http\Controllers\ContactoDonanteController@deleteC');
 Route::resource('donaciones', 'App\Http\Controllers\DonacionController');
 Route::resource('contactoDonante', 'App\Http\Controllers\ContactoDonanteController');
+
+// Rutas empleados
+Route::resource('employee', 'App\Http\Controllers\EmployeesController');
+Route::resource('employeeStatus', 'App\Http\Controllers\StatusEmployeeController');
+Route::resource('scholarship', 'App\Http\Controllers\ScholarshipController');
+Route::resource('status', 'App\Http\Controllers\EmployeesShiftsController');
+Route::resource('employeesShifts', 'App\Http\Controllers\EmployeesShiftsController');
+Route::resource('employeeJobTitles', 'App\Http\Controllers\JobTitleController');
+Route::resource('employeeCivilStatus', 'App\Http\Controllers\CivilStatusController');
+Route::resource('employeeVacations', 'App\Http\Controllers\VacationsController');
+Route::post('employeesShifts/search', 'App\Http\Controllers\EmployeesShiftsController@filterByEmployee');
+Route::post('employeesShifts/delete', 'App\Http\Controllers\EmployeesShiftsController@deleteByEmployee');
+Route::get('employee/{id}', 'App\Http\Controllers\EmployeesController@show');
+Route::get('employeeVacations/{id}', 'App\Http\Controllers\VacationsController@show');
+Route::get('employeeFiles/{id}', 'App\Http\Controllers\Employee_FilesController@show');
+Route::any('employee/exit/{id}', 'App\Http\Controllers\EmployeesController@exitEmployee');
+
+
+
+
+
+
+
+Route::post('incomes/search', 'App\Http\Controllers\IncomesController@filterByDate');
+//Rutas cuentas
 
 Route::resource('account', 'App\Http\Controllers\AccountController');
 Route::resource('accountRole', 'App\Http\Controllers\Account_RoleController');
 Route::get('account/find/{username}', 'App\Http\Controllers\AccountController@showId');
 Route::get('account/table/all', 'App\Http\Controllers\AccountController@showTable');
+Route::get('account/table/roles/{role}', 'App\Http\Controllers\AccountController@showByRole');
+Route::get('account/table/search/{keyWord}', 'App\Http\Controllers\AccountController@searchBar');
+Route::get('account/delete/information/{id}', 'App\Http\Controllers\AccountController@deleteInfo');
+
 /* EMPIEZA GENERALES */
 Route::resource('status', 'App\Http\Controllers\StatusController');
-
 Route::resource('headquarters', 'App\Http\Controllers\HeadquarterController');
 /* TERMINA GENERALES */
 
