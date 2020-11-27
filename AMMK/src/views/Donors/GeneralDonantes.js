9ca5063213ea0abca1e9a8d03001f50ced358f91
mariana394 <a01704671@itesm.mx>
2020-11-27 18:24:29 -0500

    cards

--- AMMK/src/views/Donors/GeneralDonantes.js
+++ AMMK/src/views/Donors/GeneralDonantes.js
@@ -2,6 +2,8 @@ import React, { Component } from "react";
 import Form from "react-bootstrap/Form";
 import { Link } from "react-router-dom";
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import Swal from "sweetalert2";
+import { Progress, Alert, Col, Card, CardBody, CardHeader } from "reactstrap";
 
 import Button from "react-bootstrap/Button";
 import {
@@ -11,8 +13,6 @@ import {
   } from "reactstrap";
 import Row from "react-bootstrap/Col";
 import ProgressBar from "react-bootstrap/ProgressBar";
-import Col from "react-bootstrap/Col";
-import Card from "react-bootstrap/Card";
 import { AvForm, AvField, AvGroup, AvInput, AvFeedback, AvRadioGroup, AvRadio, AvCheckboxGroup, AvCheckbox } from 'availity-reactstrap-validation';
 
 import axios from "axios";
@@ -70,10 +70,16 @@ handleInvalidSubmit(event, errors, values) {
 
 onSubmit(e) {
   e.preventDefault()
-  console.log("onSubmit");
+  //console.log("onSubmit");
   var tipoDonante=document.getElementById("selectTipoDonante").value;
   var recurrencia= document.getElementById("selectRecurrencia").value;
-
+  if (tipoDonante === '' || recurrencia === '' ){
+    Swal.fire( {
+      icon: 'error',
+      title: '¡ERROR!',
+      text: 'Verifica que todos los campos obligatorios estén completos.',
+    })
+  } else {
   const tipoDonante2={
     idTipoDonante: tipoDonante
     
@@ -86,6 +92,7 @@ localStorage.setItem("tipoDonante2", JSON.stringify(tipoDonante2));
 localStorage.setItem("recurrencia", JSON.stringify(tipoRecurrencia));
   window.location = "http://localhost:3000/admin/RegistroDonante1";
 
+}
 }
 
 
@@ -100,12 +107,14 @@ localStorage.setItem("recurrencia", JSON.stringify(tipoRecurrencia));
 
           <h1 className="title">Registrar Donante</h1>
           
-           <h3 align="center">Datos Generales</h3>
-           <h6 align="left">Los campos señalados con (*) son obligatorios</h6>
-
-          <ProgressBar now={10} />
-                  <br/>
-          <div class="container"></div>
+           <Card>
+          <CardHeader>
+          <h3 align="center" className="title">Datos Generales</h3>
+          <Progress striped color="primary" value="50"></Progress>
+          <br></br>
+          <Alert color="primary">Los campos marcados con un asterisco (*) son obligatorios.</Alert>
+          </CardHeader>
+          <CardBody>
        <Form>
         <FormGroup>
          <label>*Seleccione Tipo de Donante:</label>
@@ -134,7 +143,8 @@ localStorage.setItem("recurrencia", JSON.stringify(tipoRecurrencia));
                 
                   </Col> 
                   </Form>
-
+                  </CardBody>
+          </Card>
         </div>
       </div>
     );
