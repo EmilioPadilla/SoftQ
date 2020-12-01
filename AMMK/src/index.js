f25d15e8a3dda908c0492c51fbc558a86417a07a
EmilioPadilla <padillam_@hotmail.com>
2020-11-30 23:47:42 -0600

    pull from dev

--- AMMK/src/index.js
+++ AMMK/src/index.js
@@ -29,7 +29,7 @@ import "assets/demo/demo.css";
 import "assets/css/black-dashboard-react.css";
 import "assets/css/nucleo-icons.css";
 
-export const API_BASE_URL = 'http://localhost:8000/api/';
+export const API_BASE_URL = 'https://api.gestionammk.com/api/';
 export const FRONT_BASE_URL = 0;
 export const IMAGE_URL = 'https://test-api.ejsanchez.com.mx/public/';
 const hist = createBrowserHistory();
